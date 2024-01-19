7d5d1015f837e0caf097633ef74b8e04c7f6520c
Kshitiz Gupta <kshitizg@nvidia.com>
2024-01-19 00:18:31 +0000

    add conda and mamba scripts

---
+++ create_condaenv.sh
@@ -0,0 +1,1 @@
+mamba env create --name rapids_raft -f raft/conda/environments/bench_ann_cuda-120_arch-x86_64.yaml
---
+++ mamba_docker.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --net=host --gpus=all -v ${PWD}:/workspace -w /workspace condaforge/mambaforge:latest bash
